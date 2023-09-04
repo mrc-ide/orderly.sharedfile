71e246643ba2f4819d4cc132f3dd2065db0da72e
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-09-04 16:25:47 +0100

    Simpler utils

--- R/util.R
+++ R/util.R
@@ -1,26 +1,9 @@
-`%||%` <- function(x, y) { # nolint
-  if (is.null(x)) y else x
-}
-
-
-squote <- function(x) {
-  sprintf("'%s'", x)
-}
-
-
-vcapply <- function(...) {
-  vapply(..., FUN.VALUE = "")
-}
-
-
-data_frame <- function(...) {
-  data.frame(..., stringsAsFactors = FALSE, check.names = FALSE)
-}
-
-
+## just be lazy and haul things in from orderly2 for now:
 assert_named <- orderly2:::assert_named
 check_fields <- orderly2:::check_fields
 match_value <- orderly2:::match_value
 assert_file_exists <- orderly2:::assert_file_exists
 expand_dirs <- orderly2:::expand_dirs
 hash_file <- orderly2:::hash_file
+vcapply <- orderly2:::vcapply
+data_frame <- orderly2:::data_frame
